32f5f01c376e0d1343ff9e8a43888331fb08fab3
lvqq <nicksonlvqq@gmail.com>
2024-10-20 14:29:12 +0800

    chore: upgrade testing-library

--- packages/template-svelte-typescript/vite.config.ts
+++ packages/template-svelte-typescript/vite.config.ts
@@ -1,10 +1,11 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { svelteTesting } from '@testing-library/svelte/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), svelteTesting()],
   test: {
     testTimeout: 20000,
     environment: 'jsdom',
--- packages/template-vue-typescript/vite.config.ts
+++ packages/template-vue-typescript/vite.config.ts
@@ -1,11 +1,10 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue()],
   test: {
     testTimeout: 20000,
     environment: 'jsdom',
